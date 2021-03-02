b895b32e48a94b31aed20d8ba0346c568353100b
lkgross <profgross@gmail.com>
2021-03-02 11:40:06 -0500

    First commit

--- src/com/company/Metric.java
+++ src/com/company/Metric.java
@@ -1,5 +1,13 @@
 package com.company;
 
+/**
+ * Metric -
+ * A (utility) class that contains static methods related to metric conversion
+ * <p>
+ * Starter code Laura K. Gross, laura.gross@bridgew.edu, 3/2021
+ * <p>
+ * Completed by: [student name], [student email], [date of completion]
+ */
 public class Metric {
     public static final double FACTOR_MI_TO_KM = 1.609;
 
--- src/com/company/MetricDemo.java
+++ src/com/company/MetricDemo.java
@@ -1,18 +1,19 @@
 package com.company;
-
 import java.util.Scanner;
-import java.io.*;
 
+/**
+ * MetricDemo -
+ * A class that contains uses the Metric class
+ * <p>
+ * Starter code Laura K. Gross, laura.gross@bridgew.edu, 3/2021
+ * <p>
+ * Completed by: [student name], [student email], [date of completion]
+ */
 public class MetricDemo {
-    public static void main(String[] args) throws IOException     {
-        FileWriter fw = new FileWriter("metricOut.txt",true);
-        PrintWriter outputFile = new PrintWriter(fw);
+    public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.print("Enter a distance in miles: ");
         double miles = keyboard.nextDouble();
-        double km = Metric.milesToKilometers(miles);
-        outputFile.printf("%.2f", km);
-        outputFile.println();
-        outputFile.close();
+        System.out.printf("%12.2f miles is %12.2f kilometers.", miles, Metric.kilometersToMiles(miles));
     }
 }
